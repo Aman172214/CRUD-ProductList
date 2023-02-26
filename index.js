a9e9fbcd97db85b923bb74f48893e40fe37a0a63
Aman <Sharmaamqn@gmail.com>
2023-02-26 16:09:15 +0530

    create a seperate file for adding products and also add seed products

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+const Product = require('./product');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(()=>{
         console.log("Mongo connection open!")
     })
@@ -12,3 +13,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/test')
     })
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+
+app.listen(3000,()=>{
+    console.log("App is listening on port 3000");
+})
---
+++ seed.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Product = require('./product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(()=>{
+        console.log("Mongo connection open!")
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    }
+]
+Product.insertMany(seedProducts)
+    .then(res=>{
+        console.log(res)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
