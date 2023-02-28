9b7d8518b59d93976213f15084466f8145fb2c33
Aman <Sharmaamqn@gmail.com>
2023-02-28 22:43:44 +0530

    create a route to add new product and a form

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     })
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded({extended:true}))
 
+app.get('/products/new',(req,res)=>{
+    res.render('products/new');
+})
 app.get('/products', async (req,res)=>{
     const products = await Product.find({})
     res.render('products/index',{products})
@@ -22,6 +26,11 @@ app.get('/products/:id',async(req,res)=>{
     const {id} = req.params;
     const product = await Product.findById(id)
     res.render('products/show',{product})    
+})
+app.post('/products',async(req,res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`products/${newProduct._id}`)
 })
 
 app.listen(3000,()=>{
