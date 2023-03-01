90e6dc24dde61f2c4a71fd71cc81eb5504f9c152
Aman <Sharmaamqn@gmail.com>
2023-03-01 21:08:19 +0530

    patch the code by adding the logic to display the category of food when editing the product

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+const categories = ['fruit','vegetable','dairy']
+
 app.get('/products/new',(req,res)=>{
-    res.render('products/new');
+    res.render('products/new',{categories});
 })
 app.get('/products', async (req,res)=>{
     const products = await Product.find({})
@@ -37,7 +39,7 @@ app.post('/products',async(req,res)=>{
 app.get('/products/:id/edit',async(req,res)=>{
     const {id} = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit',{product}) 
+    res.render('products/edit',{product,categories}) 
 })
 app.put('/products/:id',async(req,res)=>{
     const {id} = req.params;
